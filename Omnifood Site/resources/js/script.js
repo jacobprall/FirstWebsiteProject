595824c5c52500b2f34d455131df00f8861eb6d8
Jacob <prall.jacob@gmail.com>
2020-07-11 11:10:39 -0700

    first commit

---
+++ Omnifood Site/resources/js/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+     /* For the sticky navigation */                                  
+    $('.js--section-features').waypoint(function(direction) {
+        if (direction == "down") {
+            $('nav').addClass('sticky');
+        } else {
+            $('nav').removeClass('sticky');
+        }
+    }, {
+      offset: '60px;'
+    });
+    
+    
+    /* Navigation scroll */
+
+});
